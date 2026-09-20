a126743e8f7157421f4f6eaf25068dc89074f5ee
Aradhana Chauhan <164392694+aradhanasch@users.noreply.github.com>
2026-09-20 17:31:06 +0530

    Enhance MongoDB connection error handling
    Added error handling to exit process on MongoDB connection failure.

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -9,6 +9,7 @@ const connectDB = async () => {
         console.log("MongoDB Connected Successfully")
     } catch (error) {
         console.log("MongoDB Connection Error: ", error)
+        process.exit(1)
     }
 }
 
